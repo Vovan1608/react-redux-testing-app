91ba728c32c41a6d42f60f44ff2d68ffc9d34181
Vladymyr Zenin <vovan1608@gmail.com>
2022-07-27 13:55:28 +0300

    add integration test

--- src/__tests__/commentsIntegration.test.js
+++ src/__tests__/commentsIntegration.test.js
@@ -1,9 +1,26 @@
+import moxios from 'moxios';
 import { render, screen, within, fireEvent } from '@testing-library/react';
 
 import Root from '../Root';
 import App from '../components/App';
+import { act } from 'react-dom/test-utils';
 
-it('shpuld fetch a list of comments', () => {
+beforeEach(() => {
+    moxios.install();
+    moxios.stubRequest('https://jsonplaceholder.typicode.com/comments', {
+        status: 200,
+        response: [
+            { name: 'Fetched #1' },
+            { name: 'Fetched #2' }
+        ]
+    });
+});
+
+afterEach(() => {
+    moxios.uninstall();
+});
+
+it('should fetch a list of comments', async () => {
     render(
         <Root>
             <App />
@@ -12,13 +29,17 @@ it('shpuld fetch a list of comments', () => {
 
     const fetchButton = screen.getByRole('button', { name: 'Fetch Comments' });
 
-    fireEvent.click(fetchButton);
+    await act(() => {
+        fireEvent.click(fetchButton);
+    });
 
     const commentList = screen.getByRole('list');
 
     const { queryAllByRole } = within(commentList);
 
-    const items = queryAllByRole(commentList, 'listitem');
+    const items = queryAllByRole('listitem');
 
-    // expect(items.length).toBe(500);
+    setTimeout(() => {
+        expect(items.length).toBe(2);
+    }, 1000);
 });
